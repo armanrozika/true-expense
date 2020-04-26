c4b6cb4e6dc81e47d9f7e24184f5076b8825b650
armanrozika <armanrozika@gmail.com>
2020-04-26 20:08:13 +0700

    add head section layout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <p>true expense</p>
+      <section className="home-head-section">
+        <p className="home-title">True Expense</p>
+        <div className="home-button-selection">
+          <button>TODAY</button>
+          <button>THIS MONTH</button>
+        </div>
+
+        <h4 className="current-amount">20.000</h4>
+      </section>
     </div>
   );
 }
