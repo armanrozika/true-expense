32e99234e5f07efe561ae3a1f045356dc1594d9e
armanrozika <armanrozika@gmail.com>
2020-04-26 21:42:17 +0700

    home layout with add and report button

--- src/App.js
+++ src/App.js
@@ -6,13 +6,41 @@ function App() {
   return (
     <div className="App">
       <section className="home-head-section">
-        <p className="home-title">True Expense</p>
+        <button className="btn-add">+</button>
+        <button className="btn-report">R</button>
+        <p className="home-title">TRUE EXPENSE v0.1</p>
         <div className="home-button-selection">
           <button>TODAY</button>
           <button>THIS MONTH</button>
         </div>
+        <h4 className="current-amount">20.000.000</h4>
+      </section>
 
-        <h4 className="current-amount">20.000</h4>
+      <section className="home-spend">
+        <div className="category-spend">
+          <h4>Food:</h4>
+          <p>15.000</p>
+        </div>
+        <div className="category-spend">
+          <h4>Snack:</h4>
+          <p>5.000</p>
+        </div>
+        <div className="category-spend">
+          <h4>Cigarette:</h4>
+          <p>20.000</p>
+        </div>
+        <div className="category-spend">
+          <h4>Kosan:</h4>
+          <p>20.000</p>
+        </div>
+        <div className="category-spend">
+          <h4>Fams:</h4>
+          <p>20.000</p>
+        </div>
+        <div className="category-spend">
+          <h4>Others:</h4>
+          <p>20.000</p>
+        </div>
       </section>
     </div>
   );
